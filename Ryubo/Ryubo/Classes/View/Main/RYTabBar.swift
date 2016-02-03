eaf8d1010dc3856b3145d5f9c6e54e97416d7da2
Ryukie <ryukiewang@qq.com>
2016-02-03 14:40:15 +0800

    搭建主界面:添加基本的四个标签

---
+++ Ryubo/Ryubo/Classes/View/Main/RYTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  RYTabBar.swift
+//  Ryubo
+//
+//  Created by 王荣庆 on 16/1/30.
+//  Copyright © 2016年 Ryukie. All rights reserved.
+//
+
+import UIKit
+
+class RYTabBar: UITabBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
