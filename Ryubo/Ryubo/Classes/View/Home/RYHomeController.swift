c3b92de31388101cf15091c078709fc063ea6b61
Ryukie <ryukiewang@qq.com>
2016-02-16 21:48:38 +0800

    获取Home数据

--- Ryubo/Ryubo/Classes/View/Home/RYHomeController.swift
+++ Ryubo/Ryubo/Classes/View/Home/RYHomeController.swift
@@ -15,17 +15,15 @@ class RYHomeController: RYBasicVisitorTVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         visitorView?.setVisitorViewWithInfo(nil, titleText: "关注一些人，你将打开新世界的大门")
-        demoAFN()
+//        demoAFN()
+        loadHomeData()
     }
 // MARK: - AFN demo
     private func demoAFN () {
         let urlString = "http://www.weather.com.cn/data/sk/101010100.html"
         //获取网络请求对象
-//        let manager = AFHTTPSessionManager()
         let manager = RYNetworkTool.sharedNetTool
-        //NSLocalizedDescription=Request failed: unacceptable content-type: text/html
         //设置反序列化支持的格式
-//        manager.responseSerializer.acceptableContentTypes?.insert("text/html")
         //使用字典进行参数传递
         //网络请求前,显示指示器
         SVProgressHUD.showInfoWithStatus("正在加载网络数据...")
@@ -36,9 +34,25 @@ class RYHomeController: RYBasicVisitorTVC {
                 SVProgressHUD.showInfoWithStatus("请检查网络...")
                 print(error)
         }
-        
-        
     }
+    
+// MARK: - 加载首页数据
+    private func loadHomeData() {
+        let dataURLString = "https://api.weibo.com/2/statuses/home_timeline.json"
+        //获取网络请求管理对象
+        let manager = RYNetworkTool.sharedNetTool
+        guard let token = RYAccountViewModel().token else {
+            print("用户未登录")
+            return
+        }
+        let parameters = ["access_token":token]
+        manager.GET(dataURLString, parameters: parameters, progress: nil , success: { (_ , result) -> Void in
+                print(result)
+            }) { (_ , error ) -> Void in
+                print(error)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
