fb36da8fed17c9defc3dfb0f92e64a3167d27272
Ryukie <ryukiewang@qq.com>
2016-02-19 15:38:13 +0800

    伪单例优化:  构造函数私有化 初步设置配图视图View

---
+++ Ryubo/Ryubo/Classes/View/Home/Cell/RYPicsView.swift
@@ -0,0 +1,21 @@
+//
+//  RYPicsView.swift
+//  Ryubo
+//
+//  Created by 王荣庆 on 16/2/19.
+//  Copyright © 2016年 Ryukie. All rights reserved.
+//
+
+import UIKit
+
+class RYPicsView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
